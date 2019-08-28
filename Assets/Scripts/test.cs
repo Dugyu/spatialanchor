2287e1221dfdb355e374e42ee49ee36717dbcf30
Dugyu <yxylyzy@126.com>
2019-08-28 15:04:55 +0800

    clean up

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -4,12 +4,7 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
-    public GameObject proto;
-    // Start is called before the first frame update
-    void Start()
-    {
-      
-    }
+
 
     // Update is called once per frame
     void Update()
